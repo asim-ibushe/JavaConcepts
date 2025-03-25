042ec3b3baaeb004238a66c20c9c3492eca16956
Digvijay Jatkar <jatkardigvijay@gmail.com>
2025-03-25 20:40:01 +0000

    exception theory and implementation to asim

---
+++ src/com/aib/exception/ExceptionE1.java
@@ -0,0 +1,12 @@
+package com.aib.exception;
+
+public class ExceptionE1 {
+
+	// below is the example of exception
+
+	public static void main(String[] args) {
+
+		System.out.println(10/0);
+		System.out.println("Hello world !!");
+	}
+}
